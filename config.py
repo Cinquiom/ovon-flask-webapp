68c1d78a7e97fc58f588f73228a7af3972e2b8e8
Hayden Lowes <hlowes@sasktel.net>
2018-01-10 03:10:39 -0600

    switched SQLalchemy config back to mySQL database access instead of local access

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ SECRET_KEY = "a super secret"
 
 # SQLAlchemy config
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dbuser:dickbutt@localhost:3306/ovon'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dbuser:dickbutt@localhost:3306/ovon'
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 

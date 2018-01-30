213fee4d6b211bdb747ad32540c986f3c4c4c827
kuzer200 <kuzer200@142.3.25.112>
2018-01-30 16:09:12 -0600

    Added single endpoint for retrieving user info

--- app/mod_auth/controllers.py
+++ app/mod_auth/controllers.py
@@ -1,3 +1,7 @@
+"""
+    Provides endpoints for the auth module.
+"""
+
 from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_user
 
@@ -53,23 +57,35 @@ def whoami():
 def forgot_password():
     return "", 204
 
+@mod_auth.route('/user/<int:user_id>/', methods=['GET'])
+def user_info(user_id):
+    if current_user.id == user_id:
+        ret = {
+            "username": current_user.username,
+            "email": current_user.email,
+            "fullname": current_user.fullname,
+            "date_created": current_user.date_created
+            }
+        return jsonify(ret) 
+    return "", 401
+
 
 #methods for user data retrieval on profile display page    
 @mod_auth.route('/getProfileEmail/', methods=['GET'])
 def getProfileEmail():
     profileInfo = User.query.filter_by(username=request.cookies.get('userName')).first()['email']
     return profileInfo, 200
-
+ 
 @mod_auth.route('/getProfileFullName/', methods=['GET'])
 def getProfileFullName():
     profileInfo = User.query.filter_by(username=request.cookies.get('userName')).first()['fullname']
     return profileInfo, 200
-    
+     
 @mod_auth.route('/getProfileCreationDate/', methods=['GET'])
 def getProfileCreationDate():
     profileInfo = User.query.filter_by(username=request.cookies.get('userName')).first()['date_created']
     creationDate = profileInfo.strftime('%d/%m/%Y')
     return creationDate, 200
-    
-    
-    
+     
+     
+     

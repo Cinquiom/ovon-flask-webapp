7058db6afa2c41850ff88d4da8f6ce67c0fa24a2
Hayden Lowes <hlowes@sasktel.net>
2018-03-07 13:58:13 -0600

    removing unnecessary angular libraries popper and ui-bootstrap
    Signed-off-by: Hayden Lowes <hlowes@sasktel.net>

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // The main 'app' object
-var OVONApp = angular.module('OVONApp', ['ui.router', 'ngRoute', 'ngCookies', 'ngSQLite', 'ui.bootstrap']);
+var OVONApp = angular.module('OVONApp', ['ui.router', 'ngRoute', 'ngCookies', 'ngSQLite']);
 
 OVONApp.constant('api', {
 	signIn: apiURL + '/auth/signin/',

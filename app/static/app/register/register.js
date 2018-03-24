8bbfc1c5e7a16de1f452c2058194ab3398774249
Eric Kuz <kuzer200@uregina.ca>
2018-03-24 15:47:14 -0600

    Made error messages dynamicly rendered

--- app/static/app/register/register.js
+++ app/static/app/register/register.js
@@ -7,38 +7,44 @@ var RegisterController = function($scope, $http, api, $route, $location) {
 	$scope.errors = {};
 	
 	$scope.validateUser = function(user) {
-		var errors = {};
+		$scope.errors = {};
+		$scope.success = ""
+		
+		if (!user) { user = {}; }
 		
 		if (!user.username) 
-			errors.username = "Username is blank";
+			$scope.errors.username = "Please enter a username.";
 		if (!user.password) 
-			errors.password = "Password is blank";
+			$scope.errors.password = "Please enter a password.";
 		
 		if (!user.email) {
-			errors.email = "Email is blank";
+			$scope.errors.email = "Please enter an email.";
 		} else if (!emailRegex.test(user.email.toString())) {
-			errors.email = "Email is not valid";
+			$scope.errors.email = "Email is not valid.";
 		}
 			
 		if (!user.fullname) 
-			errors.fullname = "Full name is blank";
-		if (user.password != user.passwordconfirm) 
-			errors.passwordconfirm = "Passwords do not match";
+			$scope.errors.fullname = "Please enter your name.";
 		
-		if (!angular.equals(errors, {})) {
-			console.log(errors);
-			alert("Invalid submission, try again.");
-		}	else {
+		if (user.password != user.passwordconfirm) 
+			$scope.errors.passwordconfirm = "Passwords do not match.";
+		
+		if (user.gender == null) 
+			$scope.errors.gender = "Please select a gender.";
+		
+		if (angular.equals($scope.errors, {})) {
 			 $http.post(api.register, user)
 			 	.then(
                   function (response) {
-                      alert("Account created successfully!");
-                      $location.path("/login");
-                      $route.reload();
+					  $scope.errors = {}
+                      $scope.success = "Account created successfully!  Redirecting to login...";
+					  setTimeout(function() {
+						  $location.path("/login");
+						  $route.reload();
+					  }, 3000);
                   },
                   function (errResponse) {
-                 	 console.log(errResponse);
-                 	 alert(errResponse.data.errorMessage);
+                 	 $scope.errors.response = errResponse.data.errorMessage;
                   }
               );
 		}

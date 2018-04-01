b911035f7633750c461ac55fcee96034a194a0aa
Brandon Hudy <hudy.brandon@gmail.com>
2018-04-01 14:45:19 -0600

    Changed profile page
    Moved picture and added bio to the information table

--- app/static/app/uploadprofilepicture/uploadprofilepicture.js
+++ app/static/app/uploadprofilepicture/uploadprofilepicture.js
@@ -2,6 +2,7 @@
 
 var UploadProfilePictureController = function($scope, $http, userPersistenceService, $route, $location, api) {
     
+        
 	$http.get('/static/json/navtop.json').then(function(response) {
 		$scope.navtop = response.data;
 	});
@@ -26,38 +27,5 @@ var UploadProfilePictureController = function($scope, $http, userPersistenceServ
         $route.reload();
     };
     
-    $scope.uploadPicture = function() {
-        $scope.errors = {};
-        
-        if(document.getElementById("avatar").files.length == 0) {
-            $scope.errors.avatar = "Please select an image";
-        }
-        
-        
-        if(angular.equals($scope.errors, {})) {
-            $http.post(api.upload, $scope.avatar)
-            .then(function(response) {               
-                    $location.path("/profile");
-                    $route.reload();
-                });
-        }
-    };
-    
-    $scope.uploadResume = function() {
-        $scope.errors = {};
-        
-        if(document.getElementById("resume").files.length == 0) {
-            $scope.errors.resume = "Please select a file";
-        }
-        
-        
-        if(angular.equals($scope.errors, {})) {
-            $http.post(api.upload, $scope.resume)
-            .then(function(response) {               
-                    $location.path("/profile");
-                    $route.reload();
-                });
-        }
-    };
         
 };
